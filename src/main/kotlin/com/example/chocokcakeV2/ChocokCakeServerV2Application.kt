06ce940e0939f98460e3f79e6010bda5d55fe59b
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-07 02:29:27 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/chocokcakeV2/ChocokCakeServerV2Application.kt
@@ -0,0 +1,11 @@
+package com.example.chocokcakeV2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChocokCakeServerV2Application
+
+fun main(args: Array<String>) {
+    runApplication<ChocokCakeServerV2Application>(*args)
+}
---
+++ src/test/kotlin/com/example/chocokcakeV2/ChocokCakeServerV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chocokcakeV2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChocokCakeServerV2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
