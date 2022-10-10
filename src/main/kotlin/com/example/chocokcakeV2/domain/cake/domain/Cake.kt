3420c50986f1165e77c5ad11e693622589fe32c6
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-11 08:26:44 +0900

    💡 CREATE :: Cake Entity

---
+++ src/main/kotlin/com/example/chocokcakeV2/domain/cake/domain/Cake.kt
@@ -0,0 +1,45 @@
+package com.example.chocokcakeV2.domain.cake.domain
+
+import com.example.chocokcakeV2.domain.auth.domain.General
+import com.example.chocokcakeV2.domain.cake.domain.type.CakeTheme
+import com.example.chocokcakeV2.domain.candle.domain.Candle
+import com.example.chocokcakeV2.global.common.entity.BaseTimeEntity
+import java.time.LocalDate
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity(name = "cake")
+class Cake(
+    id: Long?,
+    cakeTheme: CakeTheme,
+    birthDay: LocalDate,
+    user: General,
+    createdAt: LocalDateTime,
+    updatedAt: LocalDateTime?
+): BaseTimeEntity(
+    createdAt = createdAt,
+    updatedAt = updatedAt
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = id
+        protected set
+
+    @Column(name = "theme", nullable = false)
+    @Enumerated(EnumType.STRING)
+    var cakeTheme: CakeTheme = cakeTheme
+        protected set
+
+    @Column(name = "birth_day", nullable = false)
+    var birthDay: LocalDate = birthDay
+        protected set
+
+    @OneToMany(mappedBy = "cake", cascade = [CascadeType.REMOVE])
+    var candleList: MutableList<Candle> = ArrayList()
+        protected set
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: General = user
+}
