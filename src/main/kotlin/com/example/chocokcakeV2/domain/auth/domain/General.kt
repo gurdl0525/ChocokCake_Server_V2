f0c5de706b19faf99c52513153204cf6a570c42f
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-11 08:29:50 +0900

    🔨 UPDATE :: add @OneToMany Colum

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/domain/General.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/domain/General.kt
@@ -1,11 +1,14 @@
 package com.example.chocokcakeV2.domain.auth.domain
 
 import com.example.chocokcakeV2.domain.auth.domain.type.Role
+import com.example.chocokcakeV2.domain.cake.domain.Cake
 import java.time.LocalDate
 import java.time.LocalDateTime
+import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.DiscriminatorValue
 import javax.persistence.Entity
+import javax.persistence.OneToMany
 
 @Entity
 @DiscriminatorValue(value = "GENERAL")
@@ -27,14 +30,21 @@ class General(
     createdAt = createdAt,
     updatedAt = updatedAt
 ) {
-    @Column(name = "birth_day")
+    @Column(name = "birth_day", nullable = false)
     var birthDay: LocalDate = birthDay
         protected set
 
-    @Column(name = "is_ban")
+    @Column(name = "is_ban", nullable = false)
     var isBan: Boolean = false
         protected set
 
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.REMOVE])
+    var cakeList: MutableList<Cake> = ArrayList()
+        protected set
+
+    fun addCake(cake: Cake){
+        this.cakeList.add(cake)
+    }
     fun doBan(ban: Boolean){
         this.isBan = ban
         this.roleList = Role.BANED
