2856894f39b880d973f85ff0d7223ffca93dd6fd
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-11 08:27:53 +0900

    💡 CREATE :: Candle Type

---
+++ src/main/kotlin/com/example/chocokcakeV2/domain/candle/domain/type/CandleTheme.kt
@@ -0,0 +1,21 @@
+package com.example.chocokcakeV2.domain.candle.domain.type
+
+enum class CandleTheme {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    SKYBLUE,
+    BLUE,
+    PURPLE,
+    BROWN,
+    NUMBER_1,
+    NUMBER_2,
+    NUMBER_3,
+    NUMBER_4,
+    NUMBER_5,
+    NUMBER_6,
+    NUMBER_7,
+    NUMBER_8,
+    NUMBER_9,
+}
