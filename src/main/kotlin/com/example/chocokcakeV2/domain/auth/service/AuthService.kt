014d23e8ac448780079ac23b447c9432ececd3e6
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-11 14:43:11 +0900

    🎲 ADD :: admin signup

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthService.kt
@@ -1,7 +1,9 @@
 package com.example.chocokcakeV2.domain.auth.service
 
+import com.example.chocokcakeV2.domain.auth.presentation.dto.request.AdminSignUpRequest
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.GeneralSignUpRequest
 
 interface AuthService {
     fun generalSignUp(request: GeneralSignUpRequest)
+    fun admin(request: AdminSignUpRequest)
 }
