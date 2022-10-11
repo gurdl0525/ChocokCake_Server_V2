d96c3dc95a087c71658bef0a5ccb050138fd8063
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-11 14:45:24 +0900

    🔨 UPDATE :: @Configuration 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,6 @@ allOpen{
     annotation("org.springframework.data.redis.core.RedisHash")
     annotation("org.springframework.data.mongodb.core.mapping.Document")
     annotation("javax.persistence.Embeddable")
-    annotation("org.springframework.context.annotation.Configuration")
 }
 
 tasks.withType<KotlinCompile> {
