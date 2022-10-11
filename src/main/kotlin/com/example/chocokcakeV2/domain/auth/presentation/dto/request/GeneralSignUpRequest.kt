1e7d04649c5b1302969c55d6c21aa26233fe0e7e
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-11 14:48:36 +0900

    🔨 UPDATE :: birthDay 세분화

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/presentation/dto/request/GeneralSignUpRequest.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/presentation/dto/request/GeneralSignUpRequest.kt
@@ -1,14 +1,21 @@
 package com.example.chocokcakeV2.domain.auth.presentation.dto.request
 
-import java.time.LocalDate
+import javax.validation.constraints.Email
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
 import javax.validation.constraints.Pattern
-import javax.validation.constraints.Size
 
 data class GeneralSignUpRequest(
-    @field:Size(min = 2, max = 4)
     val name: String,
+    @field:Email(message = "올바른 이메일 형식이 아닙니다.")
     val accountId: String,
-    @field:Pattern(regexp = "sf")
+    @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#\$%^&*()+|=]{8,30}\$",
+        message = "비밀번호는 영소문자,대문자,숫자,특수문자 8~30자여야 합니다.")
     val password: String,
-    val birthDay: LocalDate
+    @field:Min(1, message = "1월부터 12월까지 입력 가능합니다")
+    @field:Max(12, message = "1월부터 12월까지 입력 가능합니다")
+    val monthOfBirthDay: Int,
+    @field:Min(1, message = "1일부터 31일까지 입력 가능합니다")
+    @field:Max(31, message = "1일부터 31일까지 입력 가능합니다")
+    val dayOfBirthDay: Int
 )
