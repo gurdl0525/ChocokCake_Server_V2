8661d0b1629530fc0a10baa6a7afc48565aabae2
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-11 14:47:16 +0900

    🔨 UPDATE :: isBan, birthDay Colum이동 General -> User

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/domain/General.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/domain/General.kt
@@ -5,7 +5,6 @@ import com.example.chocokcakeV2.domain.cake.domain.Cake
 import java.time.LocalDate
 import java.time.LocalDateTime
 import javax.persistence.CascadeType
-import javax.persistence.Column
 import javax.persistence.DiscriminatorValue
 import javax.persistence.Entity
 import javax.persistence.OneToMany
@@ -17,27 +16,19 @@ class General(
     name: String,
     accountId: String,
     password: String,
-    birthDay: LocalDate,
+    birthDay: LocalDate?,
     role: Role,
-    createdAt: LocalDateTime,
-    updatedAt: LocalDateTime?
+    createdAt: LocalDateTime
 ): User(
     id = id,
     name = name,
     accountId = accountId,
     password = password,
+    birthDay = birthDay,
     role = role,
     createdAt = createdAt,
-    updatedAt = updatedAt
+    updatedAt = null
 ) {
-    @Column(name = "birth_day", nullable = false)
-    var birthDay: LocalDate = birthDay
-        protected set
-
-    @Column(name = "is_ban", nullable = false)
-    var isBan: Boolean = false
-        protected set
-
     @OneToMany(mappedBy = "user", cascade = [CascadeType.REMOVE])
     var cakeList: MutableList<Cake> = ArrayList()
         protected set
