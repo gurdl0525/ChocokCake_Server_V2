6f64270ec8359066fcf0521626fffe5b097cb4ad
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-11 14:44:08 +0900

    🔨 UPDATE :: admin signup

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthServiceImpl.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthServiceImpl.kt
@@ -1,10 +1,13 @@
 package com.example.chocokcakeV2.domain.auth.service
 
+import com.example.chocokcakeV2.domain.auth.domain.Admin
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.GeneralSignUpRequest
 import com.example.chocokcakeV2.domain.auth.domain.General
 import com.example.chocokcakeV2.domain.auth.domain.User
 import com.example.chocokcakeV2.domain.auth.domain.type.Role
+import com.example.chocokcakeV2.domain.auth.presentation.dto.request.AdminSignUpRequest
 import com.example.chocokcakeV2.domain.auth.repository.UserRepository
+import com.example.chocokcakeV2.global.config.security.auth.AuthenticationFacade
 import com.example.chocokcakeV2.global.error.exception.DuplicatedMemberException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
@@ -14,21 +17,36 @@ import java.time.LocalDateTime
 class AuthServiceImpl(
     private val passwordEncoder: BCryptPasswordEncoder,
     private val generalRepository: UserRepository<General>,
-    private val userRepository: UserRepository<User>
+    private val adminRepository: UserRepository<Admin>,
+    private val userRepository: UserRepository<User>,
+    private val authenticationFacade: AuthenticationFacade
 ): AuthService{
     override fun generalSignUp(request: GeneralSignUpRequest) {
-        if(userRepository.existsByAccountId(request.accountId)){
-            generalRepository.save(General(
-                id = null,
-                name = request.name,
-                accountId = request.accountId,
-                password = passwordEncoder.encode(request.password),
-                birthDay = request.birthDay,
-                createdAt = LocalDateTime.now(),
-                updatedAt = null,
-                role = Role.GENERAL
-            ))
-        }else throw DuplicatedMemberException(request.accountId)
+        duplicateMemberVerification(request.accountId)
+        generalRepository.save(General(
+            id = null,
+            name = request.name,
+            accountId = request.accountId,
+            password = passwordEncoder.encode(request.password),
+            birthDay = authenticationFacade.getBirthDay(request.monthOfBirthDay, request.dayOfBirthDay),
+            createdAt = LocalDateTime.now(),
+            role = Role.GENERAL
+        ))
+    }
 
+    override fun admin(request: AdminSignUpRequest) {
+        duplicateMemberVerification(request.accountId)
+        adminRepository.save(Admin(
+            id = null,
+            name = request.name,
+            accountId = request.accountId,
+            password = passwordEncoder.encode(request.password),
+            createdAt = LocalDateTime.now()
+        ))
+    }
+    private fun duplicateMemberVerification(accountId: String){
+        if(userRepository.existsByAccountId(accountId)){
+            throw DuplicatedMemberException(accountId)
+        }
     }
 }
