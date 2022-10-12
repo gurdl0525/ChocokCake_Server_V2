ead72af722ca2778bf0eb180cfad3aad11b83897
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-12 10:44:59 +0900

    💡 CREATE :: TokenProperty

---
+++ src/main/kotlin/com/example/chocokcakeV2/global/config/security/jwt/dotenv/TokenProperty.kt
@@ -0,0 +1,15 @@
+package com.example.chocokcakeV2.global.config.security.jwt.dotenv
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConfigurationProperties("jwt")
+@ConstructorBinding
+data class TokenProperty(
+    val header: String,
+    val secretKey: String,
+    val accessExp: Long,
+    val accessTyp: String,
+    val refreshExp: Long,
+    val refreshTyp: String
+)
