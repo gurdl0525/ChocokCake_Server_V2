a7c14bb97f4c128963fbada5befb462514224faf
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-12 10:55:20 +0900

    💡 CREATE :: UserNotFoundException

---
+++ src/main/kotlin/com/example/chocokcakeV2/global/error/exception/UserNotFoundException.kt
@@ -0,0 +1,6 @@
+package com.example.chocokcakeV2.global.error.exception
+
+import com.example.chocokcakeV2.global.error.data.ErrorCode
+
+class UserNotFoundException(data: String): BusinessException(ErrorCode.USER_NOT_FOUND, data) {
+}
