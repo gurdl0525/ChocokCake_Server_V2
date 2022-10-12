3897a55364796adbb4278ff7efd04ca85381b3c9
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-12 10:45:54 +0900

    💡 CREATE :: UnAuthorizedException

---
+++ src/main/kotlin/com/example/chocokcakeV2/global/error/exception/UnAuthorizedException.kt
@@ -0,0 +1,6 @@
+package com.example.chocokcakeV2.global.error.exception
+
+import com.example.chocokcakeV2.global.error.data.ErrorCode
+
+class UnAuthorizedException(data: String):BusinessException(ErrorCode.UN_AUTHORIZED, data) {
+}
