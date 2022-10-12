db252133874bcfe3b37b31aa5cc152f4a46029dd
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-12 10:42:01 +0900

    💡 CREATE :: UserDetailsService 구현체

---
+++ src/main/kotlin/com/example/chocokcakeV2/global/config/security/auth/AuthDetailsService.kt
@@ -0,0 +1,20 @@
+package com.example.chocokcakeV2.global.config.security.auth
+
+import com.example.chocokcakeV2.domain.auth.domain.User
+import com.example.chocokcakeV2.domain.auth.repository.UserRepository
+import com.example.chocokcakeV2.global.error.exception.UserNotFoundException
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Component
+
+@Component
+class AuthDetailsService(
+    private val userRepository: UserRepository<User>
+): UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails {
+        return userRepository.findByAccountId(username)
+            .map { AuthDetails(user = it) }
+            .orElseThrow { UserNotFoundException("$username to token") }
+    }
+
+}
