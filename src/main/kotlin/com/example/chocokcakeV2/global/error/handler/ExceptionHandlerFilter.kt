66692e9718863af3a468f938bb5a8f93b9a24941
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-12 10:43:10 +0900

    💡 CREATE :: ExceptionHandlerFilter

---
+++ src/main/kotlin/com/example/chocokcakeV2/global/error/handler/ExceptionHandlerFilter.kt
@@ -0,0 +1,52 @@
+package com.example.chocokcakeV2.global.error.handler
+
+import com.example.chocokcakeV2.global.error.data.ErrorCode
+import com.example.chocokcakeV2.global.error.data.ErrorResponse
+import com.example.chocokcakeV2.global.error.exception.BusinessException
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class ExceptionHandlerFilter(
+    private val objectMapper: ObjectMapper
+): OncePerRequestFilter(){
+
+    @Throws(IOException::class)
+    private fun sendErrorResponse(
+        errorCode: ErrorCode,
+        data: String,
+        response: HttpServletResponse
+    ){
+        response.status = errorCode.status.value()
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+        objectMapper.writeValue(
+            response.writer,
+            ErrorResponse(
+                errorCode.message,
+                data
+            )
+        )
+    }
+
+    @Throws(IOException::class)
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try{
+            filterChain.doFilter(request, response)
+        }catch (e: BusinessException){
+            sendErrorResponse(e.errorCode, e.data, response)
+        }catch (e: Exception){
+            sendErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, "exception handler filter", response)
+        }
+    }
+}
