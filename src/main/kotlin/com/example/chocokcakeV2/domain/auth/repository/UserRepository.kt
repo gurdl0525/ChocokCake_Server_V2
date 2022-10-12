523babce39524384441118452ec5c2c2d70125f0
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-12 10:55:48 +0900

    🎲 ADD :: findByAccountId

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/repository/UserRepository.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/repository/UserRepository.kt
@@ -2,7 +2,10 @@ package com.example.chocokcakeV2.domain.auth.repository
 
 import com.example.chocokcakeV2.domain.auth.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface UserRepository<T: User>: JpaRepository<T, Long> {
     fun existsByAccountId(accountId: String): Boolean
+
+    fun findByAccountId(accountId: String): Optional<User>
 }
