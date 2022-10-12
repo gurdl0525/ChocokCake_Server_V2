55276f1d6b0be1d4d8003f418c256cfca3799e4f
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-12 10:52:27 +0900

    🔨 UPDATE :: @ConfigurationPropertiesScan추가

--- src/main/kotlin/com/example/chocokcakeV2/ChocokCakeServerV2Application.kt
+++ src/main/kotlin/com/example/chocokcakeV2/ChocokCakeServerV2Application.kt
@@ -1,9 +1,11 @@
 package com.example.chocokcakeV2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class ChocokCakeServerV2Application
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/chocokcakeV2/global/error/exception/DuplicatedMemberException.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chocokcakeV2.global.error.exception
-
-import com.example.chocokcakeV2.global.error.ErrorCode
-
-class DuplicatedMemberException(data: String): BusinessException(ErrorCode.DUPLICATED_MEMBER, data) {
-}
