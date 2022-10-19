1ffc5ba418f759869d7c2b544442174f2e829785
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-19 21:36:51 +0900

    🎲 ADD :: Login

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/presentation/controller/AuthController.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/presentation/controller/AuthController.kt
@@ -2,6 +2,8 @@ package com.example.chocokcakeV2.domain.auth.presentation.controller
 
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.AdminSignUpRequest
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.GeneralSignUpRequest
+import com.example.chocokcakeV2.domain.auth.presentation.dto.request.LoginRequest
+import com.example.chocokcakeV2.domain.auth.presentation.dto.response.TokenResponse
 import com.example.chocokcakeV2.domain.auth.service.AuthService
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
@@ -37,5 +39,14 @@ class AuthController(
         request: AdminSignUpRequest
     ){
         authService.admin(request)
+    }
+
+    @PostMapping("/login")
+    fun login(
+        @Valid
+        @RequestBody
+        request: LoginRequest
+    ): TokenResponse{
+        return authService.login(request)
     }
 }
--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthServiceImpl.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthServiceImpl.kt
@@ -1,14 +1,22 @@
 package com.example.chocokcakeV2.domain.auth.service
 
-import com.example.chocokcakeV2.domain.auth.domain.Admin
+import com.example.chocokcakeV2.domain.auth.entity.token.RefreshToken
+import com.example.chocokcakeV2.domain.auth.entity.user.Admin
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.GeneralSignUpRequest
-import com.example.chocokcakeV2.domain.auth.domain.General
-import com.example.chocokcakeV2.domain.auth.domain.User
-import com.example.chocokcakeV2.domain.auth.domain.type.Role
+import com.example.chocokcakeV2.domain.auth.entity.user.General
+import com.example.chocokcakeV2.domain.auth.entity.user.User
+import com.example.chocokcakeV2.domain.auth.entity.user.type.Role
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.AdminSignUpRequest
 import com.example.chocokcakeV2.domain.auth.repository.UserRepository
 import com.example.chocokcakeV2.global.config.security.auth.AuthenticationFacade
 import com.example.chocokcakeV2.domain.auth.exception.DuplicatedMemberException
+import com.example.chocokcakeV2.domain.auth.exception.IncorrectPassword
+import com.example.chocokcakeV2.domain.auth.presentation.dto.request.LoginRequest
+import com.example.chocokcakeV2.domain.auth.presentation.dto.response.TokenResponse
+import com.example.chocokcakeV2.domain.auth.repository.RefreshTokenRepository
+import com.example.chocokcakeV2.global.config.security.jwt.TokenProvider
+import com.example.chocokcakeV2.global.config.security.jwt.dotenv.TokenProperty
+import com.example.chocokcakeV2.global.error.exception.UserNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -19,11 +27,15 @@ class AuthServiceImpl(
     private val generalRepository: UserRepository<General>,
     private val adminRepository: UserRepository<Admin>,
     private val userRepository: UserRepository<User>,
-    private val authenticationFacade: AuthenticationFacade
+    private val authenticationFacade: AuthenticationFacade,
+    private val tokenProvider: TokenProvider,
+    private val refreshTokenRepository: RefreshTokenRepository,
+    private val tokenProperty: TokenProperty
 ): AuthService{
     override fun generalSignUp(request: GeneralSignUpRequest) {
         duplicateMemberVerification(request.accountId)
-        generalRepository.save(General(
+        generalRepository.save(
+            General(
             id = null,
             name = request.name,
             accountId = request.accountId,
@@ -31,22 +43,43 @@ class AuthServiceImpl(
             birthDay = authenticationFacade.getBirthDay(request.monthOfBirthDay, request.dayOfBirthDay),
             createdAt = LocalDateTime.now(),
             role = Role.GENERAL
-        ))
+            )
+        )
     }
 
     override fun admin(request: AdminSignUpRequest) {
         duplicateMemberVerification(request.accountId)
-        adminRepository.save(Admin(
+        adminRepository.save(
+            Admin(
             id = null,
             name = request.name,
             accountId = request.accountId,
             password = passwordEncoder.encode(request.password),
             createdAt = LocalDateTime.now()
-        ))
+            )
+        )
     }
+
     private fun duplicateMemberVerification(accountId: String){
         if(userRepository.existsByAccountId(accountId)){
             throw DuplicatedMemberException(accountId)
         }
+    }
+
+    override fun login(request: LoginRequest): TokenResponse {
+
+        val user = userRepository.findByAccountId(request.accountId)
+            .orElseThrow{UserNotFoundException(request.accountId)}
+
+        if(passwordEncoder.matches(request.password, user.password)){
+            val response = tokenProvider.generateTokens(user.accountId)
+            refreshTokenRepository.save(RefreshToken(
+                id = user.accountId,
+                accessToken = response.accessToken,
+                refreshToken = response.refreshToken,
+                tokenProperty.refreshExp
+            ))
+        }
+        throw IncorrectPassword(request.password)
     }
 }
