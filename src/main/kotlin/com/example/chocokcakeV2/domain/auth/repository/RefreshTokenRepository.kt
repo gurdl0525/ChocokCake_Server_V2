bf3cc564755edfa0859629c7e1c729c42f450790
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-21 08:20:11 +0900

    🔨 UPDATE :: Repository 삭제

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/repository/RefreshTokenRepository.kt
@@ -2,10 +2,8 @@ package com.example.chocokcakeV2.domain.auth.repository
 
 import com.example.chocokcakeV2.domain.auth.entity.token.RefreshToken
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
 import java.util.Optional
 
-@Repository
 interface RefreshTokenRepository: CrudRepository<RefreshToken, String> {
     fun findByAccessTokenAndRefreshToken(accessToken: String, refreshToken: String): Optional<RefreshToken>
 }
