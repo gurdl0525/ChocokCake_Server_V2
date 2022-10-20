96ac476eeb3c83d8e8929fe8698eb2fc88d4f817
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-20 19:10:40 +0900

    🎲 ADD :: findByAccountIdOrNull

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/repository/UserRepository.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/repository/UserRepository.kt
@@ -7,5 +7,7 @@ import java.util.Optional
 interface UserRepository<T: User>: JpaRepository<T, Long> {
     fun existsByAccountId(accountId: String): Boolean
 
-    fun findByAccountId(accountId: String): Optional<User>
+    fun findByAccountId(accountId: String): Optional<T>
+
+    fun findByAccountIdOrNull(subject: String): T?
 }
