310da5d46fd836f817ece7d8f41d7497365dc4de
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-20 19:07:47 +0900

    🔨 UPDATE :: JpaRepository -> CrudRepository

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/repository/RefreshTokenRepository.kt
@@ -1,7 +1,11 @@
 package com.example.chocokcakeV2.domain.auth.repository
 
 import com.example.chocokcakeV2.domain.auth.entity.token.RefreshToken
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.Optional
 
-interface RefreshTokenRepository: JpaRepository<RefreshToken, String> {
+@Repository
+interface RefreshTokenRepository: CrudRepository<RefreshToken, String> {
+    fun findByAccessTokenAndRefreshToken(accessToken: String, refreshToken: String): Optional<RefreshToken>
 }
