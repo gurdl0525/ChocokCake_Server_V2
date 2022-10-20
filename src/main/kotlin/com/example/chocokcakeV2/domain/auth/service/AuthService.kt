323391ce4c576e7a04b60e15598b09e9a154c3c2
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-21 08:23:42 +0900

    🔨 UPDATE :: 리펙토링

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthService.kt
@@ -1,14 +1,20 @@
 package com.example.chocokcakeV2.domain.auth.service
 
-import com.example.chocokcakeV2.domain.auth.presentation.dto.request.AdminSignUpRequest
-import com.example.chocokcakeV2.domain.auth.presentation.dto.request.GeneralSignUpRequest
-import com.example.chocokcakeV2.domain.auth.presentation.dto.request.LoginRequest
-import com.example.chocokcakeV2.domain.auth.presentation.dto.request.ReissueTokenRequest
+import com.example.chocokcakeV2.domain.auth.entity.user.User
+import com.example.chocokcakeV2.domain.auth.presentation.dto.request.*
 import com.example.chocokcakeV2.domain.auth.presentation.dto.response.TokenResponse
 
 interface AuthService {
+
+    fun checkDuplicateAccountId(request: String)
+
     fun generalSignUp(request: GeneralSignUpRequest)
+
     fun admin(request: AdminSignUpRequest)
+
     fun login(request: LoginRequest): TokenResponse
+
     fun reissue(request: ReissueTokenRequest): TokenResponse
+
+    fun deleteMember(user: User, request: WithdrawalRequest)
 }
