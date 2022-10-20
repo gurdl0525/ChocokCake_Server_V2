af1733790c2f6c5a2d7232076303a2b792dc373a
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-20 19:09:23 +0900

    🎲 ADD :: 토큰 재발급 기능 추가

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthService.kt
@@ -3,10 +3,12 @@ package com.example.chocokcakeV2.domain.auth.service
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.AdminSignUpRequest
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.GeneralSignUpRequest
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.LoginRequest
+import com.example.chocokcakeV2.domain.auth.presentation.dto.request.ReissueTokenRequest
 import com.example.chocokcakeV2.domain.auth.presentation.dto.response.TokenResponse
 
 interface AuthService {
     fun generalSignUp(request: GeneralSignUpRequest)
     fun admin(request: AdminSignUpRequest)
     fun login(request: LoginRequest): TokenResponse
+    fun reissue(request: ReissueTokenRequest): TokenResponse
 }
