c3b75c420e23f356850218dff9f77286fe733e8d
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-21 19:49:40 +0900

    🔨 UPDATE :: Refactoring

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/presentation/controller/AuthController.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/presentation/controller/AuthController.kt
@@ -1,22 +1,13 @@
 package com.example.chocokcakeV2.domain.auth.presentation.controller
 
+import com.example.chocokcakeV2.domain.auth.entity.user.User
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.*
 import com.example.chocokcakeV2.domain.auth.presentation.dto.response.TokenResponse
 import com.example.chocokcakeV2.domain.auth.service.AuthService
-import com.example.chocokcakeV2.global.error.exception.UserNotFoundException
 import org.springframework.http.HttpStatus
-import com.example.chocokcakeV2.domain.auth.entity.user.User
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
@@ -75,11 +66,8 @@ class AuthController(
     fun deleteMember(
         @Valid
         @RequestBody request: WithdrawalRequest,
-        @AuthenticationPrincipal user: User?
+        @AuthenticationPrincipal user: User
     ){
-        authService.deleteMember(
-            user?:throw UserNotFoundException("Not Found User By Token"),
-            request
-        )
+        authService.deleteMember(user, request)
     }
 }
