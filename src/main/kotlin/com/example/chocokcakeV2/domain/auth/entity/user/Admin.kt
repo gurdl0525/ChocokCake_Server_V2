640f90d385ea22c9443d03d76eb3f066e52d83fd
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-21 19:48:54 +0900

    🔨 UPDATE :: OnDelete추가

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/entity/user/Admin.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/entity/user/Admin.kt
@@ -1,12 +1,15 @@
 package com.example.chocokcakeV2.domain.auth.entity.user
 
 import com.example.chocokcakeV2.domain.auth.entity.user.type.Role
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.time.LocalDateTime
 import javax.persistence.DiscriminatorValue
 import javax.persistence.Entity
 
 @Entity
 @DiscriminatorValue(value = "ADMIN")
+@OnDelete(action = OnDeleteAction.CASCADE)
 class Admin(
     id: Long?,
     name: String,
