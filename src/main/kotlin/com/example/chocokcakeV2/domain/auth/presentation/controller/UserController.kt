6d736d87ebf2eda5a0b5be33a9fe0c0f7725f5f5
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-21 19:58:00 +0900

    💡 CREATE :: UserController

---
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/presentation/controller/UserController.kt
@@ -0,0 +1,24 @@
+package com.example.chocokcakeV2.domain.auth.presentation.controller
+
+import com.example.chocokcakeV2.domain.auth.entity.user.User
+import com.example.chocokcakeV2.domain.auth.presentation.dto.response.UserMaximumInfoResponse
+import com.example.chocokcakeV2.domain.auth.service.UserService
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/user")
+class UserController(
+    private val userService: UserService
+) {
+    @GetMapping("/info/{id}")
+    fun getMaximumMyInfo(
+        @AuthenticationPrincipal user: User,
+        @PathVariable("id") id : Long
+    ): UserMaximumInfoResponse {
+        return userService.getUserInfo(user, id)
+    }
+}
