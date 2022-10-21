fa23cadd967908f7e607b3aaac08076b6aa124b8
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-21 15:57:10 +0900

    💡 CREATE :: 영속성컨텍스트 시작을 앞당기는 설정

---
+++ src/main/kotlin/com/example/chocokcakeV2/global/config/filter/OpenEntityManagerConfiguration.kt
@@ -0,0 +1,19 @@
+package com.example.chocokcakeV2.global.config.filter
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter
+
+@Configuration
+class OpenEntityManagerConfiguration {
+
+    @Bean
+    fun openEntityManagerInViewFilter(): FilterRegistrationBean<OpenEntityManagerInViewFilter> {
+        val filterFilterRegistrationBean = FilterRegistrationBean<OpenEntityManagerInViewFilter>()
+        filterFilterRegistrationBean.filter = OpenEntityManagerInViewFilter()
+        filterFilterRegistrationBean.order = Int.MIN_VALUE
+        return filterFilterRegistrationBean
+    }
+
+}
