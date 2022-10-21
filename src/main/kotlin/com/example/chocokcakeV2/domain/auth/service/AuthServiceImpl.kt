7fd2c0d161e07ad053ae9e7965a64a31bfb27f66
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-21 19:50:06 +0900

    🔨 UPDATE :: Refactoring

--- src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthServiceImpl.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/auth/service/AuthServiceImpl.kt
@@ -1,17 +1,18 @@
 package com.example.chocokcakeV2.domain.auth.service
 
-import com.example.chocokcakeV2.domain.auth.entity.token.RefreshToken
 import com.example.chocokcakeV2.domain.auth.entity.user.Admin
 import com.example.chocokcakeV2.domain.auth.entity.user.General
 import com.example.chocokcakeV2.domain.auth.entity.user.User
 import com.example.chocokcakeV2.domain.auth.entity.user.type.Role
-import com.example.chocokcakeV2.domain.auth.repository.UserRepository
-import com.example.chocokcakeV2.global.config.security.auth.AuthenticationFacade
 import com.example.chocokcakeV2.domain.auth.exception.DuplicatedMemberException
 import com.example.chocokcakeV2.domain.auth.exception.IncorrectPasswordException
 import com.example.chocokcakeV2.domain.auth.presentation.dto.request.*
 import com.example.chocokcakeV2.domain.auth.presentation.dto.response.TokenResponse
+import com.example.chocokcakeV2.domain.auth.repository.AdminRepository
+import com.example.chocokcakeV2.domain.auth.repository.GeneralRepository
 import com.example.chocokcakeV2.domain.auth.repository.RefreshTokenRepository
+import com.example.chocokcakeV2.domain.auth.repository.UserRepository
+import com.example.chocokcakeV2.global.config.security.auth.AuthenticationFacade
 import com.example.chocokcakeV2.global.config.security.jwt.TokenProvider
 import com.example.chocokcakeV2.global.error.exception.UserNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -21,8 +22,8 @@ import java.time.LocalDateTime
 @Service
 class AuthServiceImpl(
     private val passwordEncoder: BCryptPasswordEncoder,
-    private val generalRepository: UserRepository<General>,
-    private val adminRepository: UserRepository<Admin>,
+    private val generalRepository: GeneralRepository,
+    private val adminRepository: AdminRepository,
     private val userRepository: UserRepository<User>,
     private val authenticationFacade: AuthenticationFacade,
     private val tokenProvider: TokenProvider,
