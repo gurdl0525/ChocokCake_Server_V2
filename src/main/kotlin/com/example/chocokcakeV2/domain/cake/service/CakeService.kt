4c35da48391373af41b000f9a110a0a625ae8fd2
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-30 23:12:15 +0900

    🎲 ADD :: getMaximumCake

--- src/main/kotlin/com/example/chocokcakeV2/domain/cake/service/CakeService.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/cake/service/CakeService.kt
@@ -1,8 +1,12 @@
 package com.example.chocokcakeV2.domain.cake.service
 
 import com.example.chocokcakeV2.domain.auth.entity.user.User
-import com.example.chocokcakeV2.domain.cake.presentation.dto.ThemeRequest
+import com.example.chocokcakeV2.domain.cake.presentation.dto.request.ThemeRequest
+import com.example.chocokcakeV2.domain.cake.presentation.dto.response.MaximumCakeResponse
 
 interface CakeService {
     fun generateCake(user: User, request: ThemeRequest)
+
+    fun getMaximumCake(id: Long, page: Int): MaximumCakeResponse
+
 }
