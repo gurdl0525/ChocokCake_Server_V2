d688b80d10a5c9e01615a784b805271d1ad45f04
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-30 21:55:41 +0900

    🔨 UPDATE :: ThemeRequest 인자값 추가

--- src/main/kotlin/com/example/chocokcakeV2/domain/cake/presentation/controller/CakeController.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/cake/presentation/controller/CakeController.kt
@@ -1,13 +1,11 @@
 package com.example.chocokcakeV2.domain.cake.presentation.controller
 
 import com.example.chocokcakeV2.domain.auth.entity.user.User
+import com.example.chocokcakeV2.domain.cake.presentation.dto.ThemeRequest
 import com.example.chocokcakeV2.domain.cake.service.CakeService
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/cake")
@@ -17,8 +15,9 @@ class CakeController(
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun generateCake(
-        @AuthenticationPrincipal user: User
+        @AuthenticationPrincipal user: User,
+        @RequestBody request: ThemeRequest
     ){
-        cakeService.generateCake(user)
+        cakeService.generateCake(user, request)
     }
 }
--- src/main/kotlin/com/example/chocokcakeV2/domain/cake/service/CakeService.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/cake/service/CakeService.kt
@@ -1,7 +1,8 @@
 package com.example.chocokcakeV2.domain.cake.service
 
 import com.example.chocokcakeV2.domain.auth.entity.user.User
+import com.example.chocokcakeV2.domain.cake.presentation.dto.ThemeRequest
 
 interface CakeService {
-    fun generateCake(user: User)
+    fun generateCake(user: User, request: ThemeRequest)
 }
