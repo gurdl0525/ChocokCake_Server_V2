30bbdec7c2c2f3a8f45444f1d5a44476b3cbd697
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-30 23:12:26 +0900

    🎲 ADD :: getMaximumCake

--- src/main/kotlin/com/example/chocokcakeV2/domain/cake/presentation/controller/CakeController.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/cake/presentation/controller/CakeController.kt
@@ -1,7 +1,8 @@
 package com.example.chocokcakeV2.domain.cake.presentation.controller
 
 import com.example.chocokcakeV2.domain.auth.entity.user.User
-import com.example.chocokcakeV2.domain.cake.presentation.dto.ThemeRequest
+import com.example.chocokcakeV2.domain.cake.presentation.dto.request.ThemeRequest
+import com.example.chocokcakeV2.domain.cake.presentation.dto.response.MaximumCakeResponse
 import com.example.chocokcakeV2.domain.cake.service.CakeService
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -19,5 +20,13 @@ class CakeController(
         @RequestBody request: ThemeRequest
     ){
         cakeService.generateCake(user, request)
+    }
+
+    @GetMapping()
+    fun getMaximumCake(
+        @RequestParam id: Long,
+        @RequestParam page: Int = 0
+    ): MaximumCakeResponse{
+        return cakeService.getMaximumCake(id, page)
     }
 }
