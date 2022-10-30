071480d5c71abffd1a01791c5c28f624a77d17f8
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-30 21:54:23 +0900

    💡 CREATE :: ThemeRequest

---
+++ src/main/kotlin/com/example/chocokcakeV2/domain/cake/presentation/dto/ThemeRequest.kt
@@ -0,0 +1,7 @@
+package com.example.chocokcakeV2.domain.cake.presentation.dto
+
+import com.example.chocokcakeV2.domain.cake.entity.type.CakeTheme
+
+data class ThemeRequest(
+    val theme: CakeTheme
+)
