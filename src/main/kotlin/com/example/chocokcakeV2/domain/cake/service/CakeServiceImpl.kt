3a47097f0642138c386c51ec9a4c79ade65bc530
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-30 21:56:59 +0900

    🎲 ADD :: generateCake

--- src/main/kotlin/com/example/chocokcakeV2/domain/cake/service/CakeServiceImpl.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/cake/service/CakeServiceImpl.kt
@@ -2,16 +2,44 @@ package com.example.chocokcakeV2.domain.cake.service
 
 import com.example.chocokcakeV2.domain.auth.entity.user.General
 import com.example.chocokcakeV2.domain.auth.entity.user.User
+import com.example.chocokcakeV2.domain.cake.entity.Cake
+import com.example.chocokcakeV2.domain.cake.exception.AlreadyExistCakeException
+import com.example.chocokcakeV2.domain.cake.presentation.dto.ThemeRequest
 import com.example.chocokcakeV2.domain.cake.repository.CakeRepository
+import com.example.chocokcakeV2.global.common.facade.BirthDayFacade
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Service
 class CakeServiceImpl(
-    private val cakeRepository: CakeRepository
+    private val cakeRepository: CakeRepository,
+    private val birthDayFacade: BirthDayFacade
 ): CakeService {
-    override fun generateCake(user: User) {
+
+    override fun generateCake(user: User, request: ThemeRequest) {
         if(user is General){
-            //cakeRepository.save()
+            val birthDay = user.birthDay!!
+            if(birthDay.isBefore(LocalDate.now())){
+                user.setBirthday(
+                    birthDayFacade.enterBirthDay(
+                        birthDay.month.value,
+                        birthDay.dayOfMonth
+                    )
+                )
+            }
+            val cakeList = user.cakeList
+            if(cakeList.size -1 >= 0 && (cakeList[cakeList.size -1].birthDay == user.birthDay)){
+                throw AlreadyExistCakeException(user.toString())
+            }
+            cakeRepository.save(Cake(
+                id = null,
+                cakeTheme = request.theme,
+                birthDay = user.birthDay!!,
+                user = user,
+                createdAt = LocalDateTime.now(),
+                updatedAt = null
+            ))
         }
     }
 }
