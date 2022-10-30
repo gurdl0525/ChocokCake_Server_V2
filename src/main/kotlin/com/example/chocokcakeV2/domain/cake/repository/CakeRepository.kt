980f8da3b0e624b43c60063bb2c6527edc2ba177
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-30 21:56:10 +0900

    🔨 UPDATE :: General -> Cake

--- src/main/kotlin/com/example/chocokcakeV2/domain/cake/repository/CakeRepository.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/cake/repository/CakeRepository.kt
@@ -1,7 +1,7 @@
 package com.example.chocokcakeV2.domain.cake.repository
 
-import com.example.chocokcakeV2.domain.auth.entity.user.General
+import com.example.chocokcakeV2.domain.cake.entity.Cake
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CakeRepository: JpaRepository<General, Long> {
+interface CakeRepository: JpaRepository<Cake, Long> {
 }
