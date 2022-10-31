e5c21d1ba722addf090d5e114e9b02cf25b44286
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-31 11:30:23 +0900

    🔨 UPDATE :: Refactoring

--- src/main/kotlin/com/example/chocokcakeV2/global/error/exception/UserNotFoundException.kt
+++ src/main/kotlin/com/example/chocokcakeV2/global/error/exception/UserNotFoundException.kt
@@ -2,5 +2,4 @@ package com.example.chocokcakeV2.global.error.exception
 
 import com.example.chocokcakeV2.global.error.data.ErrorCode
 
-class UserNotFoundException(data: String): BusinessException(ErrorCode.USER_NOT_FOUND, data) {
-}
+class UserNotFoundException(data: String): BusinessException(ErrorCode.USER_NOT_FOUND, data)
