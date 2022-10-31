4cf27ce03ebc6df0093ab7ed1aedcf268f5b5dd1
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-31 11:29:20 +0900

    🔨 UPDATE :: Refactoring

--- src/main/kotlin/com/example/chocokcakeV2/global/error/exception/BusinessException.kt
+++ src/main/kotlin/com/example/chocokcakeV2/global/error/exception/BusinessException.kt
@@ -2,5 +2,4 @@ package com.example.chocokcakeV2.global.error.exception
 
 import com.example.chocokcakeV2.global.error.data.ErrorCode
 
-open class BusinessException(val errorCode: ErrorCode, val data: String): RuntimeException(errorCode.message) {
-}
+open class BusinessException(val errorCode: ErrorCode, val data: String): RuntimeException(errorCode.message)
