bd4e8a7150f4682845059d20c3bb1d9bc8a53127
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-31 11:05:51 +0900

    🎲 ADD :: getMaximumCandle

--- src/main/kotlin/com/example/chocokcakeV2/domain/candle/presentation/controller/CandleController.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/candle/presentation/controller/CandleController.kt
@@ -1,14 +1,12 @@
 package com.example.chocokcakeV2.domain.candle.presentation.controller
 
+import com.example.chocokcakeV2.domain.auth.entity.user.User
 import com.example.chocokcakeV2.domain.candle.presentation.dto.request.CreateCandleRequest
+import com.example.chocokcakeV2.domain.candle.presentation.dto.response.MaximumCandleResponse
 import com.example.chocokcakeV2.domain.candle.service.CandleService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/candle")
@@ -23,5 +21,13 @@ class CandleController(
         @RequestBody request: CreateCandleRequest
     ){
         candleService.createCandle(id, request)
+    }
+
+    @GetMapping
+    fun getMaximumCandle(
+        @AuthenticationPrincipal user: User,
+        @RequestParam id: Long
+    ): MaximumCandleResponse{
+        return candleService.getMaximumCandle(user, id)
     }
 }
