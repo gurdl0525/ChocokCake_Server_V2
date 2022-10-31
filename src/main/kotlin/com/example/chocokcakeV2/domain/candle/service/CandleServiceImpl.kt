b648efbe80d15e90617ba19380725228d7aa00a4
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-31 11:06:05 +0900

    🎲 ADD :: getMaximumCandle

--- src/main/kotlin/com/example/chocokcakeV2/domain/candle/service/CandleServiceImpl.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/candle/service/CandleServiceImpl.kt
@@ -1,10 +1,15 @@
 package com.example.chocokcakeV2.domain.candle.service
 
+import com.example.chocokcakeV2.domain.auth.entity.user.Admin
+import com.example.chocokcakeV2.domain.auth.entity.user.User
 import com.example.chocokcakeV2.domain.cake.repository.CakeRepository
 import com.example.chocokcakeV2.domain.candle.entity.Candle
+import com.example.chocokcakeV2.domain.candle.exception.CandleNotFoundException
 import com.example.chocokcakeV2.domain.candle.presentation.dto.request.CreateCandleRequest
+import com.example.chocokcakeV2.domain.candle.presentation.dto.response.MaximumCandleResponse
 import com.example.chocokcakeV2.domain.candle.repository.CandleRepository
 import com.example.chocokcakeV2.global.error.exception.CakeNotFoundException
+import com.example.chocokcakeV2.global.error.exception.NoPermissionsException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -28,6 +33,20 @@ class CandleServiceImpl(
                 createdAt = LocalDateTime.now(),
                 updatedAt = null
             )
+        )
+    }
+
+    override fun getMaximumCandle(user: User, id: Long): MaximumCandleResponse {
+        val candle = candleRepository.findByIdOrNull(id)
+            ?: throw CandleNotFoundException(id.toString())
+        if(candle.cake.user != user && user !is Admin){
+            throw NoPermissionsException(user.role.toString())
+        }
+        return MaximumCandleResponse(
+            id = candle.id!!,
+            theme = candle.candleTheme,
+            postman = candle.postman,
+            letter = candle.letter
         )
     }
 
