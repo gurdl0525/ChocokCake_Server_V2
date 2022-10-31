c75660fee2ca2259242eb38e0aa62bde81d0ed8a
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-31 11:06:22 +0900

    🎲 ADD :: getMaximumCandle

--- src/main/kotlin/com/example/chocokcakeV2/domain/candle/service/CandleService.kt
+++ src/main/kotlin/com/example/chocokcakeV2/domain/candle/service/CandleService.kt
@@ -1,9 +1,13 @@
 package com.example.chocokcakeV2.domain.candle.service
 
+import com.example.chocokcakeV2.domain.auth.entity.user.User
 import com.example.chocokcakeV2.domain.candle.presentation.dto.request.CreateCandleRequest
+import com.example.chocokcakeV2.domain.candle.presentation.dto.response.MaximumCandleResponse
 
 interface CandleService {
 
     fun createCandle(id: Long, request: CreateCandleRequest)
+
+    fun getMaximumCandle(user: User, id: Long): MaximumCandleResponse
 
 }
