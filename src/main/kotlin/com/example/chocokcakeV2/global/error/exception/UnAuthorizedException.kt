9ae858dbb58ec17ca5f7f4ded3e12fb473ab4962
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-31 11:30:09 +0900

    🔨 UPDATE :: Refactoring

--- src/main/kotlin/com/example/chocokcakeV2/global/error/exception/UnAuthorizedException.kt
+++ src/main/kotlin/com/example/chocokcakeV2/global/error/exception/UnAuthorizedException.kt
@@ -2,5 +2,4 @@ package com.example.chocokcakeV2.global.error.exception
 
 import com.example.chocokcakeV2.global.error.data.ErrorCode
 
-class UnAuthorizedException(data: String):BusinessException(ErrorCode.UN_AUTHORIZED, data) {
-}
+class UnAuthorizedException(data: String):BusinessException(ErrorCode.UN_AUTHORIZED, data)
