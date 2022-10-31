6cb1d8128d71d11f482714c79074d59d61a6f148
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-31 11:29:47 +0900

    🔨 UPDATE :: Refactoring

--- src/main/kotlin/com/example/chocokcakeV2/global/error/exception/CakeNotFoundException.kt
+++ src/main/kotlin/com/example/chocokcakeV2/global/error/exception/CakeNotFoundException.kt
@@ -2,5 +2,4 @@ package com.example.chocokcakeV2.global.error.exception
 
 import com.example.chocokcakeV2.global.error.data.ErrorCode
 
-class CakeNotFoundException(data: String): BusinessException(ErrorCode.CAKE_NOT_FOUND, data) {
-}
+class CakeNotFoundException(data: String): BusinessException(ErrorCode.CAKE_NOT_FOUND, data)
