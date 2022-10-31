f02e56e368a7abc629123b54e6be5444e3f3f000
squall7011 <gurdl7011@dsm.hs.kr>
2022-10-31 11:31:07 +0900

    🎲 ADD :: NOT_YET_BIRTHDAY

--- src/main/kotlin/com/example/chocokcakeV2/global/error/data/ErrorCode.kt
+++ src/main/kotlin/com/example/chocokcakeV2/global/error/data/ErrorCode.kt
@@ -10,6 +10,7 @@ enum class ErrorCode(
     DUPLICATED_MEMBER("Duplicated Member", HttpStatus.BAD_REQUEST),
     INCORRECT_PASSWORD("Incorrect Password", HttpStatus.BAD_REQUEST),
     ALREADY_EXIST_CAKE("ALREADY EXIST CAKE", HttpStatus.BAD_REQUEST),
+    NOT_YET_BIRTHDAY("Not Yet Birthday", HttpStatus.BAD_REQUEST),
 
     //401
     UN_AUTHORIZED("Un Authorized", HttpStatus.UNAUTHORIZED),
